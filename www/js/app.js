4c4fe5eb55326c93d4cc35cb247f1c101e7fdd9d
Gary Tse <garytse89@gmail.com>
2014-02-12 16:09:19 -0800

    Don't allow user to enter blank items

--- www/js/app.js
+++ www/js/app.js
@@ -3,12 +3,14 @@
 /* jshint strict: false */
 // declares to jshint that $ is a variable
 
-var i=2; // counter``
+var i=2; // one existing item so current counter starts off at 2
 
 var inputField = '<span><input type="text" name="name" id="inputField" placeholder="Enter list item" /></span>';
 var inputButton = '<span><input type="button" value="Submit" id="inputButton"/></span>';
 
 function createListItem() {
+	if( !$('#inputField').val() ) return;
+
 	var newItem = '<div class="checkbox-'+i+'"><input type="checkbox" name="checkbox-'+i+'" id="checkbox-'+i+'" class="custom" />\
                 <label for="checkbox-'+i+'">' + $('#inputField').val() + '</label></div>';
     $('.list').append(newItem);
@@ -21,8 +23,14 @@ function createListItem() {
 }
 
 function createNewLabel() {
-	var newLabel = '<div>' + $('#inputField').val() + '</div>';
-	$('.list').append(newLabel);	
+	if( !$('#inputField').val() ) return;
+	
+	var newLabel = '<div class="label-'+i+'">' + $('#inputField').val() + '</div>';
+	$('.list').append(newLabel);
+	$( "div.label-"+i ).bind( "taphold", function(event) {
+    	$("div.label-"+(i-1)).remove(); // by the time this function runs, i will have been incremented
+    });
+  	i++;	
 }
 
 // jQuery
